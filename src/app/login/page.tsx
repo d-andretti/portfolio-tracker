b715b6702c68b3b4b836ca05716b825826ce044c
d-andretti <73660537+d-andretti@users.noreply.github.com>
2024-03-27 13:26:29 +0000

    create login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,14 @@
+import styles from "../ui/login/login.module.css";
+
 export default function LoginPage() {
-  return <div>Login Page</div>;
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
